9c2c76b17c4e9c05f3248f582b3ca6a7744df1a7
tanubha <tanubha97@gmail.com>
2020-09-14 21:45:15 +0530

    first commit

---
+++ src/main/java/com/web/app/webapp/WebAppApplication.java
@@ -0,0 +1,15 @@
+package com.web.app.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.web.app.webapp")
+public class WebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppApplication.class, args);
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/login.jsp
@@ -0,0 +1,6 @@
+<html>
+<head>
+<title>First WebPage</title>
+</head>
+<body>My first web page...</body>
+</html>
